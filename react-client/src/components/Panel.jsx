2ff479798d828e8086ea6b590ba38b022a83c954
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-04-06 18:46:11 -0700

    Got name input to be dynamic.

--- react-client/src/components/Panel.jsx
+++ react-client/src/components/Panel.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-const Panel = (props) => (
-  <div>
-    <h4>Friend Finder</h4>
-    <div>
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Location" />
-      <button>Submit</button>
-    </div>
-  </div>
-)
+class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState({name: e.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Friend Finder</h4>
+        <div>
+          <input type="text" placeholder="Name" onChange={this.handleChange.bind(this)}/>
+          <input type="text" placeholder="Location" />
+          <button>Submit</button>
+        </div>
+      </div>)
+  }
+}
 
 export default Panel;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -34,7 +34,7 @@ class App extends React.Component {
 
   render () {
     return (<div>
-      <Panel />
+      <Panel name="Jo" />
       <Map />
     </div>)
   }
